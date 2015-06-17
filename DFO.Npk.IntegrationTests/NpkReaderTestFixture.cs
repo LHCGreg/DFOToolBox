ace92972922effb3cb9dfd9c75599531d706efa3
LHCGreg <LHCGreg@server.fake>
2015-06-17 18:15:27 -0400

    Fix bug in NpkReader when reading some uncompressed frames.
    Use frame.Width * frame.Height * bytesPerPixel
    (based on the pixel format - 1555, 4444, etc), instead of
    (length from metadata) / 2. (length from metadata) / 2 was not always
    accurate.
    
    Add a diagnostic mode to NpkReader that will do extra error checks
    and fire an event when one of those checks finds an error.
    
    Add integration test project for DFO.Npk which loads every frame
    in every ImagePacks2 NPK. This takes around 13 minutes on my computer
    and is CPU bound, so there is likely some optimization that can be done.
    
    Also don't seek through the pixel data when reading .img metadata
    except in diagnostic mode.

---
+++ DFO.Npk.IntegrationTests/NpkReaderTestFixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFO.Common;
+using DFO.Common.Images;
+using NUnit.Framework;
+
+namespace DFO.Npk.IntegrationTests
+{
+    [TestFixture]
+    public class NpkReaderTestFixture
+    {
+        [Test]
+        public void ReadAllImages()
+        {
+            Config config = new Config();
+            List<string> extraErrors = new List<string>();
+            foreach (string path in Directory.GetFiles(config.ImageNpkDir, "*.NPK"))
+            {
+                using (NpkReader npk = new NpkReader(path, extraErrorHandler: (sender, args) => { extraErrors.Add(args.Message); Console.WriteLine(args.Message); }))
+                {
+                    npk.PreLoadAllSpriteFrameMetadata();
+
+                    foreach (NpkPath imgPath in npk.Frames.Keys)
+                    {
+                        IReadOnlyList<FrameInfo> imgFrames = npk.Frames[imgPath];
+                        for (int frameIndex = 0; frameIndex < imgFrames.Count; frameIndex++)
+                        {
+                            npk.GetImage(imgPath, frameIndex);
+                        }
+                    }
+
+                    if (extraErrors.Count > 0)
+                    {
+                        throw new Exception("Errors detected. Check console output for details.");
+                    }
+                }
+            }
+        }
+    }
+}
--- DFO.Npk.IntegrationTests/Properties/AssemblyInfo.cs
+++ DFO.Npk.IntegrationTests/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DFO.Npk")]
-[assembly: AssemblyDescription("Library for reading DFO's .NPK files")]
+[assembly: AssemblyTitle("DFO.Npk.IntegrationTests")]
+[assembly: AssemblyDescription("Integration tests for DFO.Npk")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DFO Toolbox")]
@@ -20,9 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fba957d2-0701-434a-bd56-b711983c7d2e")]
+[assembly: Guid("cf379e8f-6db6-4184-9cf6-c715b922e4bb")]
 
-[assembly: InternalsVisibleTo("DFO.Npk.IntegrationTests")]
-
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
