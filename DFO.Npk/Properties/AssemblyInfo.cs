740e7628ee20684446fde133b0e1dd5c73a2bc04
LHCGreg <LHCGreg@server.fake>
2015-07-14 20:45:06 -0400

    Versions up to 0.2.0.0.

--- DFO.Npk/Properties/AssemblyInfo.cs
+++ DFO.Npk/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("DFO.Npk.IntegrationTests")]
 
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
