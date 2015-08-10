68d0a2a00dfd085509ad82fb40e3bd19fe6d6c46
LHCGreg <LHCGreg@server.fake>
2015-08-10 08:32:03 -0400

    Forgot to bump versions to 0.3.0.0 for release.

--- DFO.Npk/Properties/AssemblyInfo.cs
+++ DFO.Npk/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("DFO.Npk.IntegrationTests")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
